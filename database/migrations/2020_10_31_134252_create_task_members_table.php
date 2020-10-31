8ef5ec60b5e6cf4367e5326bab9fe536a8d95498
asteriagame <asteriagame@gmail.com>
2020-11-01 00:02:02 +0300

    Добавлены таблицы в СУБД для подзадач и польз.

---
+++ database/migrations/2020_10_31_134252_create_task_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_members', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('task_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_members');
+    }
+}
---
+++ database/migrations/2020_10_31_154516_create_sub_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('task_id')->unsigned();
+            $table->string('title');
+            $table->boolean('confirmed')->default(0);
+            $table->string('start_time')->nullable(true);
+            $table->string('end_time')->nullable(true);
+            $table->timestamps();
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_tasks');
+    }
+}
